33c0ab568c866688bd6fc907ce66e3de1aeea14d
rob <u18024361@tuks.co.za>
2020-08-05 11:48:53 +0200

    s

---
+++ WebApplication1/WebApplication1/Controllers/AAAAController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class AAAAController : Controller
+    {
+        // GET: AAAA
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
