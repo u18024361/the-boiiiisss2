14a51a6f7d2fa47e5a5f55b7ed16437ae744c943
Lihsay123 <U18137319@tuks.co.za>
2020-08-05 11:57:30 +0200

    anthing

---
+++ WebApplication1/WebApplication1/Controllers/AnythingController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class AnythingController : Controller
+    {
+        // GET: Anything
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
